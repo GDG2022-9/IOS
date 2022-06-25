2a01142cf78c0725859aec613af75932e0b9a6cb
YOUNGJAE NO <noyoungj96@gmail.com>
2022-06-26 04:10:50 +0900

    [UPDATE] main

--- GDG/GDG/SceneDelegate.swift
+++ GDG/GDG/SceneDelegate.swift
@@ -17,11 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        let storyboard = UIStoryboard(name: "SignIn", bundle: nil)
-    
-        guard let SignInVC = storyboard.instantiateViewController(withIdentifier: "SignIn") as? SignInViewController else { return }
-            window?.rootViewController = SignInVC
-        
 
     }
 
